40809de7d33bc5874b81e520e321f139a83863f7
Francisco Neff <francisconeff@gmail.com>
2023-08-30 14:25:01 +0200

    test: TestCase for Hotel and Room models

--- apps/hotel/models.py
+++ apps/hotel/models.py
@@ -1,6 +1,8 @@
+from typing import Type
+from django.db.models.manager import BaseManager
 from django.utils.translation import gettext_lazy as _
 from django.db import models
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.exceptions import ValidationError
 
 from apps.account.models import Account
@@ -15,7 +17,8 @@ class Hotel(models.Model):
     stars = models.PositiveSmallIntegerField(verbose_name=_('Stars'), validators=[MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey(Account, related_name='update_by_reference', null=True, on_delete=models.SET_NULL)
+    updated_by = models.ForeignKey(Account, related_name='update_by_reference', null=True, blank=False, on_delete=models.SET_NULL)
+
 
     class Meta:
         verbose_name = _('Hotel')
@@ -24,12 +27,21 @@ class Hotel(models.Model):
     def __str__(self):
         return f'{self.id}-{self.name}'
 
+    def update_model(self, model_object=None, **extra_fields):
+        if model_object is None or not isinstance(model_object, Hotel):
+            raise ValidationError(message=_('The object can´t be updated.'))
+        
+        for field, value in extra_fields.items():
+            setattr(model_object, field, value)
 
+        model_object.save()
+        return model_object
 
 
 class HotelMedia(models.Model):
     id_hotel = models.ForeignKey(Hotel, related_name='hotel_media_reference', null=False, unique=False, blank=False, on_delete=models.CASCADE)
     img = models.ImageField(upload_to='media_hotel/')
+
 
     class Meta:
         verbose_name = _('Hotel Media')
@@ -48,44 +60,54 @@ class Room(models.Model):
         available = _('Available'), _('Available')
         discontinued = _('Discontinued'), _('Discontinued')
 
-    class ChoicesTypeRoom(models.TextChoices):
-        unknown = 'unknown','unknown'
-        single = 'single','single'
-        double = 'double','double'
-        triple = 'triple','triple'
-        quadruple = 'quadruple','quadruple'
-        suite = 'suite', 'suite'
-
     id_hotel = models.ForeignKey(Hotel, related_name='hotel_reference', on_delete=models.RESTRICT)
     name = models.CharField(verbose_name=_('Room name'), max_length=250, null=True, unique=False, blank=True)
     description = models.TextField(verbose_name=_('Description'), max_length=5000, null=True, unique=False, blank=True)
     number = models.PositiveSmallIntegerField(verbose_name=_('Number'))
     room_status = models.CharField(verbose_name=_('Status of room'), max_length=15, null=False, unique=False, choices=ChoicesStatusRoom.choices, default=ChoicesStatusRoom.available)
     price = models.DecimalField(verbose_name=_('Price per night'), unique=False, max_digits=6, decimal_places=2, default=0)
-    room_type = models.CharField(verbose_name=_('Type of room'), max_length=15, null=False, unique=False, choices=ChoicesTypeRoom.choices, default=ChoicesTypeRoom.unknown)
+    room_capacity = models.PositiveSmallIntegerField(verbose_name=_('Guest Capacity'), validators=[MinValueValidator(1), MaxValueValidator(10)], default=1)
+    num_bed = models.PositiveSmallIntegerField(verbose_name=_('Number of Beds'), validators=[MinValueValidator(1), MaxValueValidator(12)], default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey(Account, related_name='update_by_room_reference', null=True, on_delete=models.SET_NULL)
+    updated_by = models.ForeignKey(Account, related_name='update_by_room_reference', null=True, blank=False, on_delete=models.SET_NULL)
    
+
     class Meta:
         verbose_name = _('Room')
         verbose_name_plural = _('Rooms')
         constraints = [models.UniqueConstraint(fields=['name', 'number', 'id_hotel'], name="unique_name_number_per_hotel") ]
     
     def __str__(self):
-        if self.name:
-            return f'{self.id}-{self.room_type}-{self.name}'
+        if self.name and self.number:
+            return f'{self.id}-C{self.room_capacity}-B{self.num_bed}-{self.name}-{self.number}'
+        elif self.name:
+            return f'{self.id}-C{self.room_capacity}-B{self.num_bed}-{self.name}'
         elif self.number:
-            return f'{self.id}-{self.room_type}-{self.number}'
+            return f'{self.id}-C{self.room_capacity}-B{self.num_bed}-{self.number}'
         else:
-            return f'{self.id}-{self.room_type}'
+            return f'{self.id}-C{self.room_capacity}-B{self.num_bed}'
     
     def clean_fields(self, exclude=None):
         if self.name is None and self.number is None:
             raise ValidationError(message=_('You must add the "name" field or the "number" field.'))
         return super().clean_fields(exclude)
 
+    def update_model(self, model_object=None, **extra_fields):
+        if model_object is None or not isinstance(model_object, Room):
+            raise ValidationError(message=_('The object can´t be updated.'))
+        
+        for field, value in extra_fields.items():
+            setattr(model_object, field, value)
 
+        model_object.save()
+        return model_object
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    
 
 
 class RoomMedia(models.Model):
--- apps/hotel/tests.py
+++ apps/hotel/tests.py
@@ -1,3 +1,160 @@
+import time
+from faker import Faker
+
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from apps.account.tests import test_generate_account_data
+from apps.hotel.models import Account, Hotel, Room
 
 # Create your tests here.
+fake = Faker()
+LOCAL_URL = 'http://127.0.0.1:8000'
+PASSWORD = '12345678*Abc'
+
+
+def test_generate_hotel_data(self, account):
+    data_object = {
+        'name' : fake.name(),
+        'address' : fake.address(),
+        'description' : fake.sentence(),
+        'stars' : fake.pyint(max_value=5),
+        'updated_by' : account
+    }
+    return data_object
+
+
+def test_generate_room_data(self, hotel, account, room_status=Room.ChoicesStatusRoom.available):
+    data_object = {
+        'id_hotel' : hotel,
+        'name' : fake.name(),
+        'description' : fake.sentence(),
+        'number' : fake.pyint(min_value=1, max_value=1500),
+        'room_status' : room_status,
+        'price' : fake.pydecimal(left_digits=4, right_digits=2, positive=True),
+        'room_capacity' : fake.pyint(min_value=1, max_value=10),
+        'num_bed' : fake.pyint(min_value=1, max_value=12),
+        'updated_by' : account
+    }
+    return data_object
+
+
+
+
+class HotelTestCase(TestCase):
+    """
+    Test to verify the creation of Hotel model objects.
+    """
+    @classmethod
+    def setUpClass(self):
+        self.start_time = time.time()
+        super().setUpClass()
+        print(f"\nStarting the testing class: {self.__name__}")
+    
+    @classmethod
+    def tearDownClass(self):
+        super().tearDownClass()
+        print(f"\nFinishing the testing class: {self.__name__}, Elapsed time: {(time.time()-self.start_time)}" )
+    
+    def setUp(self):
+        self.model = Hotel
+        self.account = Account.objects.create_staff(**test_generate_account_data(self, is_active=True))
+        self.data_object = test_generate_hotel_data(self, account=self.account)
+    
+    def test_correct_create_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        self.assertIsNotNone(model_object.created_at)
+        self.assertIsNotNone(model_object.updated_at)
+        self.assertEqual(model_object.updated_by, self.account)
+    
+    def test_correct_update_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        self.assertIsNotNone(model_object.created_at)
+        data_object_upt = self.data_object.copy()
+        data_object_upt['name'] = fake.name()
+        model_object = self.model.update_model(self, model_object=model_object, **data_object_upt)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        self.assertEqual(model_object.name, data_object_upt['name'])
+        self.assertNotEqual(model_object.name, self.data_object['name'])
+        self.assertIsNotNone(model_object.created_at)
+        self.assertIsNotNone(model_object.updated_at)
+        self.assertNotEqual(model_object.created_at, model_object.updated_at)
+        self.assertEqual(model_object.updated_by, self.account)
+
+    def test_correct_delete_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        model_object.delete()
+        self.assertFalse(self.model.objects.filter(id=model_object.id).exists())
+
+
+
+
+class RoomTestCase(TestCase):
+    """
+    Test to verify the creation of Hotel model objects.
+    """
+    @classmethod
+    def setUpClass(self):
+        self.start_time = time.time()
+        super().setUpClass()
+        print(f"\nStarting the testing class: {self.__name__}")
+    
+    @classmethod
+    def tearDownClass(self):
+        super().tearDownClass()
+        print(f"\nFinishing the testing class: {self.__name__}, Elapsed time: {(time.time()-self.start_time)}" )
+    
+    def setUp(self):
+        self.model = Room
+        self.account = Account.objects.create_staff(**test_generate_account_data(self, is_active=True))
+        self.hotel =  Hotel.objects.create(**test_generate_hotel_data(self, account=self.account))
+        self.data_object = test_generate_room_data(self, hotel=self.hotel, account=self.account)
+    
+    def test_correct_create_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        self.assertIsNotNone(model_object.created_at)
+        self.assertIsNotNone(model_object.updated_at)
+        self.assertEqual(model_object.id_hotel, self.hotel)
+        self.assertEqual(model_object.updated_by, self.account)
+    
+    def test_incorrect_create_model(self):
+        self.data_object.pop('name')
+        self.data_object.pop('number')
+        with self.assertRaises(ValidationError) as error: 
+            self.model.objects.create(**self.data_object)
+        self.assertIsInstance(error.exception, ValidationError)
+    
+    def test_correct_update_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        data_object_upt = self.data_object.copy()
+        data_object_upt['name'] = fake.name()
+        model_object = self.model.update_model(self, model_object=model_object, **data_object_upt)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        self.assertEqual(model_object.name, data_object_upt['name'])
+        self.assertNotEqual(model_object.name, self.data_object['name'])
+        self.assertIsNotNone(model_object.created_at)
+        self.assertIsNotNone(model_object.updated_at)
+        self.assertNotEqual(model_object.created_at, model_object.updated_at)
+        self.assertEqual(model_object.id_hotel, self.hotel)
+        self.assertEqual(model_object.updated_by, self.account)
+    
+    def test_incorrect_update_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        data_object_upt = self.data_object.copy()
+        data_object_upt['name'] = None
+        data_object_upt['number'] = None
+        with self.assertRaises(ValidationError) as error: 
+            self.model.update_model(self, model_object=model_object, **data_object_upt)
+        self.assertIsInstance(error.exception, ValidationError)
+    
+    def test_correct_delete_model(self):
+        model_object = self.model.objects.create(**self.data_object)
+        self.assertTrue(self.model.objects.filter(id=model_object.id).exists())
+        model_object.delete()
+        self.assertFalse(self.model.objects.filter(id=model_object.id).exists())
